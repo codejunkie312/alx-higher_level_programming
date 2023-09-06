c10c7dcbd53f8d5a55bbc2570f5853384fa054ad
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-09-06 01:50:46 +0100

    0x01 Task 16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    if a < b:
+    if (a < b):
         return c
-    elif b > 0:
+    elif (c > b):
         return a + b
     else:
-        return a * b - c
+        return (a * b) - c
