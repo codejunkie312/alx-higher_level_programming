07236e5cabe8c432f76584fa5ebe067016addce5
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-09-14 03:13:05 +0100

    Fixed trail issue

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,53 +1,62 @@
 #include "Python.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
 /**
- * print_python_bytes - prints info about python bytes
+ * print_python_list - prints info about python bytes
  * @p: pointer to PyObject
- */
-void print_python_bytes(PyObject *p)
-{
-    Py_ssize_t size, i;
-    char *str;
-
-    printf("[.] bytes object info\n");
-    if (!PyBytes_Check(p))
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        return;
-    }
-    size = ((PyVarObject *)p)->ob_size;
-    str = ((PyBytesObject *)p)->ob_sval;
-    printf("  size: %ld\n", size);
-    printf("  trying string: %s\n", str);
-    if (size < 10)
-        printf("  first %ld bytes:", size + 1);
-    else
-        printf("  first 10 bytes:");
-}
-
-/**
- * print_python_list - prints info about python lists
- * @p: pointer to PyObject
+ * Return: void
  */
 void print_python_list(PyObject *p)
 {
-    Py_ssize_t size, alloc, i;
-    PyObject *obj;
+	long int size, i;
+	PyObject *item;
 
-    printf("[*] Python list info\n");
-    if (!PyList_Check(p))
-    {
-        printf("  [ERROR] Invalid List Object\n");
-        return;
-    }
-    size = ((PyVarObject *)p)->ob_size;
-    alloc = ((PyListObject *)p)->allocated;
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", alloc);
+	printf("[*] Python list info\n");
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
 }
 
+/**
+ * print_python_bytes - prints some basic info about Python bytes.
+ * @p: Pointer to PyObject
+ * Return: void
+ */
+void print_python_bytes(PyObject *p)
+{
+	long int size, i;
+	char *bytes_content;
 
+	printf("[*] Python bytes\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+	size = PyBytes_Size(p);
+	bytes_content = PyBytes_AsString(p);
+	printf("[*] Size of the Python Bytes = %ld\n", size);
+	printf("[*] Trying string: %s\n", bytes_content);
+
+	if (size < 10)
+		printf("[*] First %ld bytes: ", size);
+	else
+		printf("[*] First 10 bytes: ");
+
+	for (i = 0; i < size && i < 10; i++)
+	{
+		printf("%02hhx", bytes_content[i]);
+		if (i < 9 && i < size - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
