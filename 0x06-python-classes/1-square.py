a9deee0a9f1e9077445b1d0a9e3889af89c04a68
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-09-26 23:12:30 +0100

    0x06 Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains the class Square with attributes:
+* Private instance attribute: _size
+"""
+
+
+class Square:
+    """
+    This is the Square class defining a square with attribute size
+    """
+    def __init__(self, size):
+        self.__size = size
