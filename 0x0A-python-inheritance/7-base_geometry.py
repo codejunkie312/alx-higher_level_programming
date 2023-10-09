d7b1906ed415c1c6a97dd38a270499df8c1079a3
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-10-09 19:54:56 +0100

    0x0A Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A module that contains the definition of the class BaseGeometry."""
+
+
+class BaseGeometry:
+    """definition of the BaseGeometry class.
+
+    Attributes:
+
+
+    Methods:
+        area(self)
+        integer_validator(self, name, value)
+    """
+
+    def area(self):
+        """A function that raises an exception
+
+        Raises:
+            Exception: always with message "area() is not implemented"
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A function that validate 'value'.
+
+        Args:
+            name: name attached to the value.
+            value (int): a positive integer value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
