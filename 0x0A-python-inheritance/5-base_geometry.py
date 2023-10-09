952e5ae79d55ff3ac2bfeebaa0e4feed01918a45
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-10-09 19:42:13 +0100

    0x0A Task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module that contains the definition of the class BaseGeometry."""
+
+
+class BaseGeometry:
+    """definition of the BaseGeometry class.
+
+    Attributes:
+
+
+    Methods:
+    """
+    pass
