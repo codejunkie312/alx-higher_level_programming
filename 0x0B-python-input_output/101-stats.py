0c987b4d12e2e9784db26f4d52a370eb8253628a
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-10-12 00:58:36 +0100

    another fix

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -11,6 +11,7 @@ status_code_counts = {
     "403": 0, "404": 0, "405": 0, "500": 0
     }
 line_count = 0
+flag = False
 
 
 def print_stats(file_size, status_code_counts):
@@ -21,25 +22,30 @@ def print_stats(file_size, status_code_counts):
         status_code_counts (dict): A dictionary containing the status
         codes and their counts.
     """
+    global flag
     print("File size: {:d}".format(file_size))
     for key in sorted(status_code_counts.keys()):
         if status_code_counts[key] != 0:
             print("{:s}: {:d}".format(key, status_code_counts[key]))
+    flag = True
 
 
 try:
     for line in sys.stdin:
-        try:
-            line_count += 1
-            split_line = line.split()
-            total_size += int(split_line[-1])
-            status_code = split_line[-2]
-            if status_code in status_code_counts.keys():
-                status_code_counts[status_code] += 1
-            if line_count % 10 == 0:
-                print_stats(total_size, status_code_counts)
-        except Exception:
+        line_count += 1
+        split_line = line.split()
+        if len(split_line) < 2:
             continue
+        total_size += int(split_line[-1]) if split_line[-1].isdigit() else 0
+        status_code = split_line[-2]
+        if status_code in status_code_counts.keys():
+            status_code_counts[status_code] += 1
+            flag = False
+        if line_count % 10 == 0:
+            print_stats(total_size, status_code_counts)
 except KeyboardInterrupt:
     print_stats(total_size, status_code_counts)
     raise
+finally:
+    if not flag:
+        print_stats(total_size, status_code_counts)
