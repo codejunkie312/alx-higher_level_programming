21f87ca4d2a527dbd40d81ddaaa5a1b1e6abbd2d
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-10-18 18:45:10 +0100

    revamped the tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,3 +1,4 @@
+"""Unittest for base.py."""
 import unittest
 from unittest import mock
 from models.base import Base
@@ -6,63 +7,64 @@ from models.square import Square
 import json
 import os
 
-class TestBase(unittest.TestCase):
 
-    def setUp(self):
-        Base._Base__nb_objects = 0  # Resetting the __nb_objects to 0 before each test
+class TestBase_init(unittest.TestCase):
+    """Unit tests for testing instantiation of the Base class."""
 
-    def test_init_with_id(self):
-        b1 = Base(89)
-        self.assertEqual(b1.id, 89)
-
-    def test_init_without_id(self):
+    def test_no_arg(self):
+        b1 = Base()
         b2 = Base()
-        self.assertEqual(b2.id, 1)
+        self.assertEqual(b1.id, b2.id - 1)
 
-    def test_init_invalid_id(self):
-        with self.assertRaises(TypeError):
-            Base("string")
+    def test_three_bases(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 2)
+
+    def test_None_id(self):
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_with_arg(self):
+        b1 = Base(12)
+        b2 = Base(2)
+        self.assertEqual(b1.id, 12)
+        self.assertEqual(b2.id, 2)
+
+class TestBase_to_json_string(unittest.TestCase):
+    """Unit tests for testing to_json_string method of Base class."""
 
     def test_to_json_string(self):
-        dict_list = [{"id": 1}, {"id": 2}]
-        self.assertEqual(Base.to_json_string(dict_list), json.dumps(dict_list))
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(type(json_dictionary), str)
+
+    def test_to_json_string_empty(self):
+        json_dictionary = Base.to_json_string([])
+        self.assertEqual(json_dictionary, "[]")
+
+    def test_to_json_string_None(self):
+        json_dictionary = Base.to_json_string(None)
+        self.assertEqual(json_dictionary, "[]")
+
+class TestBase_from_json_string(unittest.TestCase):
+    """Unit tests for testing from_json_string method of Base class."""
 
     def test_from_json_string(self):
-        json_str = '[{"id": 1}, {"id": 2}]'
-        self.assertEqual(Base.from_json_string(json_str), [{"id": 1}, {"id": 2}])
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        dictionary_list = Base.from_json_string(json_dictionary)
+        self.assertEqual(type(dictionary_list), list)
+        self.assertEqual(dictionary_list[0]['x'], 2)
+    
+    def test_from_json_string_empty(self):
+        dictionary_list = Base.from_json_string("")
+        self.assertEqual(dictionary_list, [])
 
-    def test_save_to_file(self):
-        b1 = Square(1)
-        b2 = Square(2)
-        Square.save_to_file([b1, b2])
-        self.assertTrue(os.path.exists("Square.json"))
-
-    def test_load_from_file(self):
-        b1 = Square(1)
-        b2 = Square(2)
-        Square.save_to_file([b1, b2])
-        objs = Square.load_from_file()
-        self.assertIsInstance(objs, list)
-        self.assertIsInstance(objs[0], Base)
-        self.assertEqual(objs[0].id, 1)
-
-    @mock.patch('builtins.open', mock.mock_open())
-    @mock.patch('csv.writer')
-    def test_save_to_file_csv(self, mock_csv_writer):
-        b1 = Square(1)
-        b2 = Square(2)
-        Square.save_to_file_csv([b1, b2])
-        mock_csv_writer.assert_called_once()
-
-    @mock.patch('builtins.open', mock.mock_open(read_data='id\n1\n2\n'))
-    @mock.patch('csv.reader')
-    def test_load_from_file_csv(self, mock_csv_reader):
-        mock_csv_reader.return_value = iter([["id"], ["1"], ["2"]])
-        objs = Square.load_from_file_csv()
-        self.assertIsInstance(objs, list)
-        self.assertIsInstance(objs[0], Base)
-        self.assertEqual(objs[0].id, 1)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_from_json_string_None(self):
+        dictionary_list = Base.from_json_string(None)
+        self.assertEqual(dictionary_list, [])
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,61 +1,279 @@
 import unittest
+from unittest.mock import patch
+from io import StringIO
+import sys
 from models.rectangle import Rectangle
 
-class TestRectangle(unittest.TestCase):
+class TestRectangle_init(unittest.TestCase):
+    """Unit tests for testing instantiation of the Rectangle class."""
 
-    def setUp(self):
-        Rectangle._Base__nb_objects = 0
+    def test_no_arg(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle()
+    
+    def test_one_arg(self):
+        with self.assertRaises(TypeError):
+            r = Rectangle(1)
+    
+    def test_two_args(self):
+        r1 = Rectangle(1, 2)
+        r2 = Rectangle(2, 1)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, r2.id - 1)
 
-    def test_init(self):
+    def test_three_args(self):
+        r1 = Rectangle(1, 2, 3)
+        r2 = Rectangle(2, 1, 3)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, r2.id - 1)
+    
+    def test_four_args(self):
+        r1 = Rectangle(1, 2, 3, 4)
+        r2 = Rectangle(2, 1, 3, 4)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.y, 4)
+        self.assertEqual(r1.id, r2.id - 1)
+    
+    def test_five_args(self):
+        r1 = Rectangle(1, 2, 3, 4, 5)
+        r2 = Rectangle(2, 1, 3, 4, 5)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.y, 4)
+        self.assertEqual(r1.id, 5)
+        self.assertNotEqual(r1.id, r2.id - 1)
+    
+
+class TestRectangle_width(unittest.TestCase):
+    """Unit tests for testing width setter and getter of Rectangle class."""
+
+    def test_width_getter(self):
         r = Rectangle(1, 2)
         self.assertEqual(r.width, 1)
+    
+    def test_width_setter(self):
+        r = Rectangle(1, 2)
+        r.width = 3
+        self.assertEqual(r.width, 3)
+    
+    def test_width_setter_type_error(self):
+        r = Rectangle(1, 2)
+        with self.assertRaises(TypeError):
+            r.width = "3"
+    
+    def test_width_setter_value_error(self):
+        r = Rectangle(1, 2)
+        with self.assertRaises(ValueError):
+            r.width = -3
+            r.width = 0
+
+class TestRectangle_height(unittest.TestCase):
+    """Unit tests for testing height setter and getter of Rectangle class."""
+
+    def test_height_getter(self):
+        r = Rectangle(1, 2)
         self.assertEqual(r.height, 2)
-
-    def test_id(self):
-        r = Rectangle(3, 2, id=89)
-        self.assertEqual(r.id, 89)
-
-    def test_width_type_error(self):
+    
+    def test_height_setter(self):
+        r = Rectangle(1, 2)
+        r.height = 3
+        self.assertEqual(r.height, 3)
+    
+    def test_height_setter_type_error(self):
+        r = Rectangle(1, 2)
         with self.assertRaises(TypeError):
-            r = Rectangle("1", 2)
+            r.height = "3"
+    
+    def test_height_setter_value_error(self):
+        r = Rectangle(1, 2)
+        with self.assertRaises(ValueError):
+            r.height = -3
+            r.height = 0
 
-    def test_height_type_error(self):
-        with self.assertRaises(TypeError):
-            r = Rectangle(1, "2")
+class TestRectangle_x(unittest.TestCase):
+    """Unit test for testing x setter and getter of Rectangle class."""
 
-    def test_x_type_error(self):
+    def test_x_getter(self):
+        r = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r.x, 3)
+    
+    def test_x_setter(self):
+        r = Rectangle(1, 2, 3, 4)
+        r.x = 5
+        self.assertEqual(r.x, 5)
+    
+    def test_x_setter_type_error(self):
+        r = Rectangle(1, 2, 3, 4)
         with self.assertRaises(TypeError):
-            r = Rectangle(1, 2, x="0")
+            r.x = "5"
+    
+    def test_x_setter_value_error(self):
+        r = Rectangle(1, 2, 3, 4)
+        with self.assertRaises(ValueError):
+            r.x = -5
 
-    def test_y_type_error(self):
+class TestRectangle_y(unittest.TestCase):
+    """Unit test for testing y setter and getter of Rectangle class."""
+
+    def test_y_getter(self):
+        r = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r.y, 4)
+    
+    def test_y_setter(self):
+        r = Rectangle(1, 2, 3, 4)
+        r.y = 5
+        self.assertEqual(r.y, 5)
+    
+    def test_y_setter_type_error(self):
+        r = Rectangle(1, 2, 3, 4)
         with self.assertRaises(TypeError):
-            r = Rectangle(1, 2, y="0")
+            r.y = "5"
+    
+    def test_y_setter_value_error(self):
+        r = Rectangle(1, 2, 3, 4)
+        with self.assertRaises(ValueError):
+            r.y = -5
+
+class TestRectangle_area(unittest.TestCase):
+    """Unit tests for testing area method of Rectangle class."""
 
     def test_area(self):
         r = Rectangle(2, 3)
         self.assertEqual(r.area(), 6)
 
-    def test_update_args(self):
-        r = Rectangle(1, 1, 1, 1, 1)
-        r.update(2, 2, 2, 2, 2)
-        self.assertEqual(r.id, 2)
-        self.assertEqual(r.width, 2)
-        self.assertEqual(r.height, 2)
-        self.assertEqual(r.x, 2)
-        self.assertEqual(r.y, 2)
+class TestRectangle_display(unittest.TestCase):
+    """Unit tests for testing display method of Rectangle class."""
 
-    def test_update_kwargs(self):
-        r = Rectangle(1, 1, 1, 1, 1)
-        r.update(id=2, width=2, height=2, x=2, y=2)
-        self.assertEqual(r.id, 2)
-        self.assertEqual(r.width, 2)
-        self.assertEqual(r.height, 2)
-        self.assertEqual(r.x, 2)
-        self.assertEqual(r.y, 2)
+    def test_display(self):
+        r = Rectangle(2, 3)
+        expected_output = "##\n##\n##\n"
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            r.display()
+            self.assertEqual(fake_stdout.getvalue(), expected_output)
+
+    def test_display_with_x(self):
+        r = Rectangle(2, 3, 2)
+        expected_output = "  ##\n  ##\n  ##\n"
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            r.display()
+            self.assertEqual(fake_stdout.getvalue(), expected_output)
+    
+    def test_display_with_y(self):
+        r = Rectangle(2, 3, 0, 2)
+        expected_output = "\n\n##\n##\n##\n"
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            r.display()
+            self.assertEqual(fake_stdout.getvalue(), expected_output)
+    
+    def test_display_with_x_and_y(self):
+        r = Rectangle(2, 3, 2, 2)
+        expected_output = "\n\n  ##\n  ##\n  ##\n"
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            r.display()
+            self.assertEqual(fake_stdout.getvalue(), expected_output)
+    
+class TestRectangle_str(unittest.TestCase):
+    """Unit tests for testing __str__ method of Rectangle class."""
+
+    def test_str(self):
+        r = Rectangle(1, 2, 3, 4, 5)
+        expected_output = "[Rectangle] (5) 3/4 - 1/2\n"
+        with patch('sys.stdout', new=StringIO()) as fake_stdout:
+            print(r)
+            self.assertEqual(fake_stdout.getvalue(), expected_output)
+    
+class TestRectangle_update_args(unittest.TestCase):
+    """Unit tests for testing update method of Rectangle class with *args."""
+
+    def test_update_args(self):
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(2, 2, 2, 2, 2)
+        r1.update(2, 2, 2, 2, 2)
+        self.assertEqual(r1.id, 2)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 2)
+        self.assertEqual(r1.y, 2)
+        self.assertEqual(r1.id, r2.id)
+
+    def test_update_args_no_args(self):
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(2, 2, 2, 2, 2)
+        r1.update()
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 1)
+        self.assertEqual(r1.x, 1)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.id, r2.id - 1)
+        
+    def test_update_args_one_arg(self):
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(2, 2, 2, 2, 2)
+        r1.update(2)
+        self.assertEqual(r1.id, 2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 1)
+        self.assertEqual(r1.x, 1)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.id, 2)
+    
+    def test_update_args_two_args(self):
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(2, 2, 2, 2, 2)
+        r1.update(2, 2)
+        self.assertEqual(r1.id, 2)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.height, 1)
+        self.assertEqual(r1.x, 1)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.id, r2.id)
+    
+    def test_update_args_three_args(self):
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(2, 2, 2, 2, 2)
+        r1.update(2, 2, 2)
+        self.assertEqual(r1.id, 2)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 1)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.id, r2.id)
+
+    def test_update_args_four_args(self):
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(2, 2, 2, 2, 2)
+        r1.update(2, 2, 2, 2)
+        self.assertEqual(r1.id, 2)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 2)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.id, 2)
+
+    def test_update_named_args(self):
+        r1 = Rectangle(1, 1, 1, 1, 1)
+        r2 = Rectangle(2, 2, 2, 2, 2)
+        r1.update(id=2, width=2, height=2, x=2, y=2)
+        self.assertEqual(r1.id, 2)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 2)
+        self.assertEqual(r1.y, 2)
+        self.assertEqual(r1.id, r2.id)
+
+class TestRectangle_to_dictionary(unittest.TestCase):
+    """Unit tests for testing to_dictionary method of Rectangle class."""
 
     def test_to_dictionary(self):
         r = Rectangle(1, 1, 1, 1, 1)
         self.assertEqual(r.to_dictionary(), {'id': 1, 'width': 1, 'height': 1, 'x': 1, 'y': 1})
-
-if __name__ == '__main__':
-    unittest.main()
