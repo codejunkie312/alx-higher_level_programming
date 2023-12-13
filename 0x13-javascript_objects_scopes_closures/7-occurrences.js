bf4b0b26489646e69656043d596bc407edb16e28
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-12-13 02:46:19 +0100

    added task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const nbOccurences = (list, searchElement) => {
+  let count = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      count += 1;
+    }
+  }
+  return count;
+}
+module.exports.nbOccurences = nbOccurences;
