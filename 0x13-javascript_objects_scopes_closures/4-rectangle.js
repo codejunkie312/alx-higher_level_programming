20811b785fcd9332d4a1eb56c046bb4e26ba969c
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-12-13 02:40:23 +0100

    added task 5

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,28 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0) {
-        return;
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return;
     }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
+    this.width = w;
+    this.height = h;
+  }
 
-    rotate () {
-        let temp = this.width;
-        this.width = this.height;
-        this.height = temp;
-    }
-
-    double () {
-        this.width *= 2;
-        this.height *= 2;
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
-  module.exports = Rectangle;
-  
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
