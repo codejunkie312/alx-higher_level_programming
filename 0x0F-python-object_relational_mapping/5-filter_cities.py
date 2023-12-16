d45736a1824b8334d5a88594150162c9d4b9d370
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-12-16 20:29:59 +0100

    added task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A script that takes in the name of a state as an
+argument and lists all and pulls from hbtn_0e_4_usa"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = "SELECT cities.name FROM cities\
+        INNER JOIN states ON states.id = cities.state_id\
+            WHERE states.name = %s\
+                ORDER BY cities.id ASC"
+    cur.execute(query, (sys.argv[4],))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
+    db.close()
