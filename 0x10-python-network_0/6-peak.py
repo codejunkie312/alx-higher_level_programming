c1beede69ed0332e49d4b8323e873d86574e74e1
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-01-06 00:53:13 +0100

    added task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 """Find a peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """Find a peak in a list of unsorted integers."""
+    if not list_of_integers:
+        return None
+    return max(list_of_integers)
