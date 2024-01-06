87e300376d680c02db68494e817d82a88c470084
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2024-01-06 01:05:40 +0100

    added more tasks

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,4 +6,14 @@ def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
-    return max(list_of_integers)
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
